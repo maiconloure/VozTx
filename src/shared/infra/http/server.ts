a904b96fc51c30f92158095920e6755d5ccdcb71
Maicon Lourenço <maiconloure@gmail.com>
2021-05-09 20:27:39 -0300

    fix PORT and HOST

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,9 +1,6 @@
 import app from "./app";
 import env from '../../../config/env'
 
-const host = env.HOST || 'http://localhost'
-const port = env.PORT || '8080'
-
-app.listen(8080, () => {
-    console.info(`⚡️⚡️⚡️[SPEECH_TO_TEXT_API]⚡️⚡️⚡️: Server is running at ${host}:${port}`)
+app.listen(env.PORT, () => {
+    console.info(`⚡️⚡️⚡️[SPEECH_TO_TEXT_API]⚡️⚡️⚡️: Server is running at ${env.HOST}:${env.PORT}`)
 })
