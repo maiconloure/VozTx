830c345197864f65d20debdcfefa5308f33f94af
Maicon Lourenço <maiconloure@gmail.com>
2021-05-09 21:09:47 -0300

    add index.html

--- src/shared/infra/http/routes/router.ts
+++ src/shared/infra/http/routes/router.ts
@@ -1,9 +1,9 @@
-import { Request, Response, Application } from 'express'
+import path from 'path'
+import { Application } from 'express'
 import  { speechToTextController } from '../../../../modules/SpeechToText/useCases/STT';
-
 export default class Routes {
     public routes(app: Application): void {
-        app.route('/SpeechToText').get((req, res) => speechToTextController.index(req, res));
-        app.route('/STT').get((req, res) => { res.sendStatus(200)});
+        app.route('/SpeechToText').get((req, res) => speechToTextController.index(req, res))
+        app.route('/').get((req, res) => { res.sendFile(path.join(__dirname, '../../../../../index.html')) })
     }
 }
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,6 +1,6 @@
 import app from "./app";
 import env from '../../../config/env'
 
-app.listen(env.PORT, () => {
+app.listen(env.PORT || 8080, () => {
     console.info(`⚡️⚡️⚡️[SPEECH_TO_TEXT_API]⚡️⚡️⚡️: Server is running at ${env.HOST}:${env.PORT}`)
 })
